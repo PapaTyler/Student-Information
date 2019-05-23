24f479d25447a33a89c2a9f35d00dfed9a3f4240
PapaTyler <imbatyler3k@gmail.com>
2019-05-23 21:01:55 +0300

    created a list of students

--- Vue/app.js
+++ Vue/app.js
@@ -1,1 +1,13 @@
+const students = [
+    {name: 'Александр', secondname: 'Туголуков', age: '33', stud_group: 'ИВТ-2-16'},
+    {name: 'Василий', secondname: 'Петров', age: '19', stud_group: 'ИВТ-1-19'}
+]
 
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        students: students,
+        message: 'Список студентов!'
+    }
+})
